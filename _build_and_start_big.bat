65147cfebc689384a0981c320f299830f2271ae0
tveijola <48431562+tveijola@users.noreply.github.com>
2020-05-25 16:25:47 +0300

    Delete _build_and_start_big.bat

--- _build_and_start_big.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd src
-gcc find_frequent_words.c file_io/file_io.c hash_table/hash_table.c word_mod/word_mod.c linked_list/linked_list.c -Wall -o ../_start_program
-
-cd ..
-_start_program.exe "big.txt"
